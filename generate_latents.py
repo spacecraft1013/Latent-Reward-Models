4cdd07b31d50485166086febb8ff276bbdc36dc3
Amin Boukari <ambouk3@umd.edu>
2025-09-18 14:21:31 -0400

    Bug fix

--- generate_latents.py
+++ generate_latents.py
@@ -8,23 +8,25 @@ from torchvision import transforms
 from wan.modules.vae import WanVAE
 
 INPUTDIR = "data/inputs"
+CHECKPOINT = "./Wan2.1-T2V-1.3B/Wan2.1_VAE.pth"
 
-vae = WanVAE(vae_pth="./Wan2.1-T2V-1.3B/Wan2.1_VAE.pth", device="cuda")
+vae = WanVAE(vae_pth=CHECKPOINT, device="cuda")
 for filename in os.listdir(INPUTDIR):
     input_path = os.path.join(INPUTDIR, filename)
     latent_path = os.path.join("data/latents", filename.split(".")[0] + ".pt")
     recon_path = os.path.join("data/reconstructions", filename)
     input_img = Image.open(input_path).convert("RGB").resize((1280, 720))
 
-    img_transforms = transforms.Compose(
-        [transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
-    )
+    img_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize([0.5], [0.5])
+    ])
 
     input_data = img_transforms(input_img).to(torch.float32).cuda()
     input_data = input_data.unsqueeze(1)
 
     latents = vae.encode([input_data])
-    torch.save(latent_path, latents[0])
+    torch.save(latents[0], latent_path)
 
     orig = vae.decode(latents)[0].squeeze(1)
 
